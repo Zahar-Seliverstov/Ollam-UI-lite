47b68d41c051d2a9b49ed04a08bafa8b58f234e6
Suchmypin <153616076+Zahar-Seliverstov@users.noreply.github.com>
2025-01-26 20:59:05 +0300

    add structure

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { continueConversation, Message } from "./actions";
+import { readStreamableValue } from "ai/rsc";
+import InputField from "@/components/InputField";
+
+export default function Home() {
+  const [conversation, setConversation] = useState<Message[]>([]);
+
+  const handleSendMessage = async (userMessage: string) => {
+    const { messages, newMessage } = await continueConversation([
+      ...conversation,
+      { role: "user", content: userMessage },
+    ]);
+
+    let textContent = "";
+
+    for await (const delta of readStreamableValue(newMessage)) {
+      textContent = `${textContent}${delta}`;
+
+      setConversation([
+        ...messages,
+        { role: "assistant", content: textContent },
+      ]);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {conversation.map((message, index) => (
+          <div key={index}>
+            {message.role}: {message.content}
+          </div>
+        ))}
+      </div>
+
+      <InputField onSendMessage={handleSendMessage} />
+    </div>
+  );
+}
